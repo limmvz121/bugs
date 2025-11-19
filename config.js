e99f6fcb17a98df0785f8cbe1b54ead0f8dbe947
wings <limmvzbusines@gmail.com>
2025-11-19 11:42:10 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,7 @@
+//credit By Limzzzhama 
+//no hapus credit ketauan hapus hitam//
+
+module.exports = {
+  domain: "https://v4.pnlprivate.jhonaley.net", // Ubah jadi Domain panel Mu !!!
+  port: "3620" // Ubah Jadi Port Panel Mu !!!
+};
