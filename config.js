908b2725cfb14921c4c2b487b66adcd98fdb4654
wings <limmvzbusines@gmail.com>
2025-11-19 11:45:43 +0800

    Delete config.js

--- config.js
+++
@@ -1,7 +0,0 @@
-//credit By Limzzzhama 
-//no hapus credit ketauan hapus hitam//
-
-module.exports = {
-  domain: "https://v4.pnlprivate.jhonaley.net", // Ubah jadi Domain panel Mu !!!
-  port: "3620" // Ubah Jadi Port Panel Mu !!!
-};
